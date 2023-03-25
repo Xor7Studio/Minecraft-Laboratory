34e7d032cd806bd35cdd8cadd34e18a2d73e7471
MC_XiaoHei <100807593+MC-XiaoHei@users.noreply.github.com>
2023-03-25 09:14:09 +0800

    update vol.001

--- Vol.001 高版本的带宽占用真的大吗？/main.js
+++ Vol.001 高版本的带宽占用真的大吗？/main.js
@@ -1,1 +1,13 @@
-console.log("AAA"); 
+const mineflayer = require('mineflayer')
+const mineflayerViewer = require('prismarine-viewer').mineflayer
+function main(){
+    const bot = mineflayer.createBot({
+        host: 'localhost',
+        port: 25565,
+        username: 'Bot'
+    });
+    bot.once('spawn', () => {
+        mineflayerViewer(bot, { port: 1109 });
+    });
+}
+main();
